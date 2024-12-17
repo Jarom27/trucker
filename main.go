1554201d8e28bfbc10f753391f4d03933d0a6b6e
Jarom Mariscal <jaromero_marz234@outlook.com>
2024-12-17 15:26:26 -0700

    Separarate responsabilities and putting the goroutines in main function

--- main.go
+++ main.go
@@ -5,22 +5,36 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 	"trucker/handlers"
 
 	"github.com/streadway/amqp"
+)
+
+var (
+	raw_message      = make(chan []byte, 1024)
+	protocol_message = make(chan handlers.Procotol_Message, 1024)
+	aproved_messages = make(chan handlers.Procotol_Message, 1024)
+	response_gps     = make(chan handlers.Response_GPS, 1024)
+	location_reports = make(chan handlers.Location_report, 1024)
+	leavings         = make(chan interface{}, 1024)
 )
 
 func main() {
 	log.SetPrefix("main(): ")
 	file, err := os.OpenFile("logs/errors.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	log.SetOutput(file)
-
+	var port = os.Getenv("TRUCKER_PORT")
 	if err != nil {
 		log.Fatal("it was not possible to load the log system ", err)
 		return
 	}
 
-	listener, err := net.Listen("tcp", ":7700")
+	if port == "" {
+		port = "7700"
+	}
+
+	listener, err := net.Listen("tcp", "0.0.0.0:"+port)
 	if err != nil {
 		log.Fatal("it was not possible to start the server, error: ", err)
 		return
@@ -30,6 +44,11 @@ func main() {
 	defer file.Close()
 
 	fmt.Println("Server listening on: ", listener.Addr())
+	go handlers.IdentifyProtocol(raw_message, protocol_message)
+
+	go handlers.ValidateMessage(protocol_message, aproved_messages)
+
+	go handlers.Process(aproved_messages, response_gps, location_reports, leavings)
 
 	for {
 		conn, err := listener.Accept()
@@ -41,10 +60,30 @@ func main() {
 		go handleConnection(conn)
 
 	}
+	close(response_gps)
+	close(location_reports)
+	close(raw_message)
+	close(protocol_message)
+	close(aproved_messages)
+	close(leavings)
+	fmt.Println("The chanels were closed with success")
 
 }
+func connectToRabbitMQ() (*amqp.Connection, error) {
+	var conn *amqp.Connection
+	var err error
+	for i := 0; i < 5; i++ { // Reintentar 5 veces
+		conn, err = amqp.Dial("amqp://guest:guest@queue:5672/")
+		if err == nil {
+			return conn, nil
+		}
+		log.Printf("Retrying RabbitMQ connection in 3 seconds... (%d/5)", i+1)
+		time.Sleep(3 * time.Second)
+	}
+	return nil, fmt.Errorf("failed to connect to RabbitMQ after retries: %w", err)
+}
 func publishToRabbitMQ(location handlers.Location_report) {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := connectToRabbitMQ()
 	if err != nil {
 		log.Fatalf("Failed to connect to RabbitMQ: %s", err)
 	}
@@ -94,21 +133,6 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	buffer := make([]byte, 1024)
-
-	//Channels
-	raw_message := make(chan []byte, 1024)
-	protocol_message := make(chan handlers.Procotol_Message, 1024)
-	aproved_messages := make(chan handlers.Procotol_Message, 1024)
-	response_gps := make(chan handlers.Response_GPS, 1024)
-	location_reports := make(chan handlers.Location_report, 1024)
-	leavings := make(chan interface{}, 1024)
-
-	go handlers.IdentifyProtocol(raw_message, protocol_message)
-
-	go handlers.ValidateMessage(protocol_message, aproved_messages)
-
-	go handlers.Process(aproved_messages, response_gps, location_reports, leavings)
-
 	//Response manager
 	go func() {
 		for {
@@ -144,11 +168,5 @@ func handleConnection(conn net.Conn) {
 		fmt.Printf("Receiving this message from GPS %x\n", buffer[:n])
 		raw_message <- buffer[:n]
 	}
-	fmt.Println("Closing the chanels")
-	close(response_gps)
-	close(location_reports)
-	close(raw_message)
-	close(protocol_message)
-	close(aproved_messages)
-	close(leavings)
+
 }
